4765d67fac20158200e2ba5e411af05c3dc9e92a
Odonno <bottiau.david@laposte.net>
2019-01-10 10:13:55 +0100

    feat(selector): handle routing with react-router

--- presentation/index.tsx
+++ presentation/index.tsx
@@ -1,39 +1,50 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import { PresentationWithI18n as BeginningPresentation } from './01-beginning';
 
 const presentations = [
     {
+        key: 'introduction',
         name: 'Introduction',
         component: BeginningPresentation
     },
     {
+        key: 'virtual-dom',
         name: 'Virtual DOM'
     },
     {
+        key: 'state-management-with-redux',
         name: 'State management with Redux'
     },
     {
+        key: 'routing',
         name: 'Routing'
     },
     {
+        key: 'rxjs-react',
         name: 'rxjs + react'
     },
     {
+        key: 'react-and-web-api',
         name: 'React & web API'
     },
     {
+        key: 'styling',
         name: 'Styling (CSS-in-JS)',
         displayName: <div>Styling <br /> (CSS-in-JS)</div>
     },
     {
+        key: 'sharing-ui-libraries',
         name: 'Sharing UI libraries (Storybook)'
     },
     {
+        key: 'advanced-and-preview-features',
         name: 'Advanced & preview features'
     }
 ];
 
-const PresentationSelector = ({ handlePresentationChange }) => (
+const PresentationSelector = () => (
     <div style={{
         backgroundColor: '#282c34',
         height: '100vh',
@@ -54,9 +65,9 @@ const PresentationSelector = ({ handlePresentationChange }) => (
             {presentations.map(p => {
                 const isAvailable = !!p.component;
                 return (
-                    <div
-                        key={p.name}
-                        onClick={() => handlePresentationChange(p)}
+                    <Link
+                        to={'/' + p.key}
+                        key={p.key}
                         style={{
                             backgroundColor: 'white',
                             padding: '40px',
@@ -69,42 +80,26 @@ const PresentationSelector = ({ handlePresentationChange }) => (
                             display: 'flex',
                             justifyContent: 'center',
                             flexDirection: 'column',
-                            cursor: 'pointer'
+                            cursor: 'pointer',
+                            textDecoration: 'none'
                         }}
                     >
                         {p.displayName || p.name}
-                    </div>
+                    </Link>
                 );
             })}
         </div>
     </div>
 );
 
-export class Presentations extends React.Component<{}, any> {
-    constructor(props) {
-        super(props);
+export const Presentations = () => (
+    <Router>
+        <div>
+            <Route path="/" exact component={PresentationSelector} />
 
-        this.state = {
-            currentPresentation: null
-        };
-    }
-
-    handlePresentationChange = (presentation) => {
-        this.setState({
-            currentPresentation: presentation
-        });
-    };
-
-    render() {
-        if (this.state.currentPresentation && this.state.currentPresentation.component) {
-            const PresentationComponent = this.state.currentPresentation.component;
-            return <PresentationComponent />;
-        }
-
-        return (
-            <PresentationSelector
-                handlePresentationChange={this.handlePresentationChange}
-            />
-        );
-    }
-};
+            {presentations.map(p => 
+                <Route key={p.key} path={'/' + p.key} component={p.component} />
+            )}
+        </div>
+    </Router>
+);
