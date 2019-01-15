0f2814e3be5ee4081f8450ecd375a6a196c80f08
Odonno <bottiau.david@laposte.net>
2019-01-15 11:02:52 +0100

    refactor(rxjs): remove useless slides

--- presentation/05-rxjs-react/index.tsx
+++ presentation/05-rxjs-react/index.tsx
@@ -73,9 +73,8 @@ const Presentation = ({ t }) => (
                 {t('What you will learn')}
             </Heading>
             <List>
-                <ListItem margin="20px 0" textSize="40px">{t('Handling UI events')}</ListItem>
+                <ListItem margin="20px 0" textSize="40px">{t('Handling UI events with observable')}</ListItem>
                 <ListItem margin="20px 0" textSize="40px">{t('Combining Redux and rxjs')}</ListItem>
-                <ListItem margin="20px 0" textSize="40px">{t('Replacing Redux with rxjs')}</ListItem>
             </List>
         </Slide>
 
@@ -232,27 +231,6 @@ const configureStore = () => {
                 <LiveEditor readOnly={true} style={{ fontSize: 26 }} />
             </LiveProvider>
         </Slide>
-
-        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
-            <Heading size={6} textColor="secondary" caps>
-                {t('Replacing Redux with rxjs')} - state$
-            </Heading>
-            <Text>TODO</Text>
-        </Slide>
-
-        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
-            <Heading size={6} textColor="secondary" caps fit>
-                {t('Replacing Redux with rxjs')} - action$
-            </Heading>
-            <Text>TODO</Text>
-        </Slide>
-
-        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
-            <Heading size={6} textColor="secondary" caps fit>
-                {t('Replacing Redux with rxjs')} - component$
-            </Heading>
-            <Text>TODO</Text>
-        </Slide>
     </Deck>
 );
 
@@ -263,9 +241,8 @@ export const resources = {
         translation: {
             "Create fully reactive applications by combining react & rxjs": "Create fully reactive applications by combining react & rxjs",
             "What you will learn": "What you will learn",
-            "Handling UI events": "Handling UI events",
+            "Handling UI events with observable": "Handling UI events with observable",
             "Combining Redux and rxjs": "Combining Redux & rxjs",
-            "Replacing Redux with rxjs": "Replacing Redux with rxjs",
             "Creating observables": "Creating observables",
             "Using observables": "Using observables",
             "initialization": "initialization"
@@ -275,9 +252,8 @@ export const resources = {
         translation: {
             "Create fully reactive applications by combining react & rxjs": "Créer des applications 100% réactives à l'aide de react & rxjs",
             "What you will learn": "Objectif",
-            "Handling UI events": "Gestion des UI events",
+            "Handling UI events with observable": "Gestion des UI events avec des observables",
             "Combining Redux and rxjs": "Combiner Redux & rxjs",
-            "Replacing Redux with rxjs": "Remplacer Redux par rxjs",
             "Creating observables": "Création des observables",
             "Using observables": "Utilisation des observables",
             "initialization": "initialisation"
