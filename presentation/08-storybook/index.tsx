5924ff314648c90a7666f895512d0885b17159fc
Odonno <bottiau.david@laposte.net>
2019-01-17 09:48:01 +0100

    feat(storybook): display storybook examples

--- presentation/08-storybook/index.tsx
+++ presentation/08-storybook/index.tsx
@@ -72,7 +72,28 @@ const Presentation = ({ t }) => (
         </Slide>
 
         <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
-            <Image src={images.storybookIntroduction} />
+            <Image src={images.storybookIntroduction} height={500} />
+        </Slide>
+
+        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
+            <iframe
+                src="https://release-3-4--storybooks-official.netlify.com/?knob-Dollars=12.5&knob-Name=Storyteller&knob-Text=Menu%20link%20item&knob-Years%20in%20NY=9&knob-background=%23ffff00&knob-Age=70&knob-Items%5B0%5D=Laptop&knob-Items%5B1%5D=Book&knob-Items%5B2%5D=Whiskey&knob-Color=%23ffff00&knob-Other%20Fruit=lime&knob-Container%20width=90&knob-Account%20Balance=12.5&knob-Birthday=1484870400000&knob-Nice=true&knob-Styles=%7B%22border%22%3A%223px%20solid%20%23ff00ff%22%2C%22padding%22%3A%2210px%22%7D&knob-Fruit=apple&knob-Active=true&selectedKind=ui%2FSearchBox&selectedStory=with%20stories&full=1&addons=0&stories=0&panelRight=0&addonPanel=storybook%2Fstories%2Fstories-panel"
+                style={{ width: 1000, maxWidth: 'none', height: 600, border: 0, borderRadius: 4, overflow: 'hidden' }}
+            />
+        </Slide>
+
+        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
+            <iframe
+                src="https://release-3-4--storybooks-official.netlify.com/?knob-Dollars=12.5&knob-Name=Storyteller&knob-Text=Menu%20link%20item&knob-Years%20in%20NY=9&knob-background=%23ffff00&knob-Age=70&knob-Items%5B0%5D=Laptop&knob-Items%5B1%5D=Book&knob-Items%5B2%5D=Whiskey&knob-Color=%23ffff00&knob-Other%20Fruit=lime&knob-Container%20width=90&knob-Account%20Balance=12.5&knob-Birthday=1484870400000&knob-Nice=true&knob-Styles=%7B%22border%22%3A%223px%20solid%20%23ff00ff%22%2C%22padding%22%3A%2210px%22%7D&knob-Fruit=apple&knob-Active=true&selectedKind=ui%2FSearchBox&selectedStory=with%20stories&full=0&addons=1&stories=0&panelRight=0&addonPanel=storybook%2Fstories%2Fstories-panel"
+                style={{ width: 1000, maxWidth: 'none', height: 600, border: 0, borderRadius: 4, overflow: 'hidden' }}
+            />
+        </Slide>
+
+        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
+            <iframe
+                src="https://release-3-4--storybooks-official.netlify.com/?knob-Dollars=12.5&knob-Name=Storyteller&knob-Text=Menu%20link%20item&knob-Years%20in%20NY=9&knob-background=%23ffff00&knob-Age=70&knob-Items%5B0%5D=Laptop&knob-Items%5B1%5D=Book&knob-Items%5B2%5D=Whiskey&knob-Color=%23ffff00&knob-Other%20Fruit=lime&knob-Container%20width=90&knob-Account%20Balance=12.5&knob-Birthday=1484870400000&knob-Nice=true&knob-Styles=%7B%22border%22%3A%223px%20solid%20%23ff00ff%22%2C%22padding%22%3A%2210px%22%7D&knob-Fruit=apple&knob-Active=true&selectedKind=ui%2FSearchBox&selectedStory=with%20stories&full=0&addons=0&stories=1&panelRight=0&addonPanel=storybook%2Fstories%2Fstories-panel"
+                style={{ width: 1000, maxWidth: 'none', height: 600, border: 0, borderRadius: 4, overflow: 'hidden' }}
+            />
         </Slide>
     </Deck>
 );
