038b5640f7278b424b36830c72808f941ad30b27
Odonno <bottiau.david@laposte.net>
2019-01-17 10:14:06 +0100

    feat(advanced): create "what you will learn" slide

--- presentation/09-advanced-preview-features/index.tsx
+++ presentation/09-advanced-preview-features/index.tsx
@@ -54,6 +54,62 @@ const Presentation = ({ t }) => (
                 {t('Advanced & preview features')}
             </Heading>
         </Slide>
+
+        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
+            <Heading size={6} textColor="secondary" caps>
+                {t('What you will learn')}
+            </Heading>
+            <List>
+                <ListItem margin="20px 0" textSize="40px">{t('Advanced component lifecycle')}</ListItem>
+                <ListItem margin="20px 0" textSize="40px">{t('React DevTools')}</ListItem>
+                <ListItem margin="20px 0" textSize="40px">{t('Server Side Rendering')}</ListItem>
+                <ListItem margin="20px 0" textSize="40px">{t('Context Provider')}</ListItem>
+                <ListItem margin="20px 0" textSize="40px">{t('Lazy loading of components')}</ListItem>
+                <ListItem margin="20px 0" textSize="40px">{t('React hooks')}</ListItem>                
+            </List>
+        </Slide>
+
+        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
+            <Heading size={6} textColor="secondary" caps>
+                {t('Advanced component lifecycle')}
+            </Heading>
+            <Text>TODO</Text>
+        </Slide>
+
+        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
+            <Heading size={6} textColor="secondary" caps>
+                {t('React DevTools')}
+            </Heading>
+            <Text>TODO</Text>
+        </Slide>
+
+        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
+            <Heading size={6} textColor="secondary" caps>
+                {t('Server Side Rendering')}
+            </Heading>
+            <Text>TODO</Text>
+        </Slide>
+
+        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
+            <Heading size={6} textColor="secondary" caps>
+                {t('Context Provider')}
+            </Heading>
+            <Text>TODO</Text>
+        </Slide>
+
+        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
+            <Heading size={6} textColor="secondary" caps>
+                {t('Lazy loading of components')}
+            </Heading>
+            <Text>TODO</Text>
+        </Slide>
+
+        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
+            <Heading size={6} textColor="secondary" caps>
+                {t('React hooks')}
+            </Heading>
+            <Text>TODO</Text>
+        </Slide>
     </Deck>
 );
 
@@ -62,12 +118,26 @@ export const PresentationWithI18n = withI18n()(Presentation);
 export const resources = {
     en: {
         translation: {
-            "Advanced & preview features": "Advanced & preview features"
+            "Advanced & preview features": "Advanced & preview features",
+            "What you will learn": "What you will learn",
+            "Advanced component lifecycle": "Advanced component lifecycle",
+            "React DevTools": "React DevTools",
+            "Server Side Rendering": "Server Side Rendering",
+            "Context Provider": "Context Provider",
+            "Lazy loading of components": "Lazy loading of components",
+            "React hooks": "React hooks"
         }
     },
     fr: {
         translation: {
-            "Advanced & preview features": "Advanced & preview features"
+            "Advanced & preview features": "Advanced & preview features",
+            "What you will learn": "Objectif",
+            "Advanced component lifecycle": "Advanced component lifecycle",
+            "React DevTools": "React DevTools",
+            "Server Side Rendering": "Server Side Rendering",
+            "Context Provider": "Context Provider",
+            "Lazy loading of components": "Lazy loading of components",
+            "React hooks": "React hooks"
         }
     }
 };
